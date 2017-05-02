e1292be1010063c342fbcbfe3a4dfd28d00213e7
natan-mlk <adam.milko@gmail.com>
2017-05-02 22:52:55 +0200

    working on form validation - wip

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,35 @@
 //Navigation effect
 $(window).on('scroll', function () {
-  var scrollTop = $(window).scrollTop();
-  if (scrollTop > $('header').height()) {
+  var $scrollTop = $(window).scrollTop();
+  if ($scrollTop > $('header').height()) {
     $('.navigation').addClass("nav-background");
   }
-  if (scrollTop < $('header').height()) {
+  if ($scrollTop < $('header').height()) {
     $('.navigation').removeClass("nav-background");
   }
 });
 
 //Form validation
 
+$('.sign-up').submit(function (event) {
+  event.preventDefault();
+  var $email = $('input[name="email"]').val();
+  //console.log($email)
+
+  if (validate($email) === false) {
+    $('.form-control').addClass('input-error');
+    $('.mail-error').fadeOut();
+  }
+});
+
+function validate(email) {
+  var pattern = /^\S+@\S+[\.][0-9a-z]+$/;
+  return pattern.test(email);
+}
+
+/*
+
+$('input[name="email"]').focusin(function () {
+  $('.mail-error').fadeOut();
+  $('.form-control').removeClass('input-error');
+});*/
